6a514b18ef1b393faafe4559d685763e7e29c1e3
LucasPorcheron <129221574+LucasPorcheron@users.noreply.github.com>
2024-06-12 16:36:53 +0200

    add card event

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,4 +21,37 @@ export const navLinks = [
     },
 ];
 
+
+
+export const events = [
+
+    {
+        col: "col-start-1",
+        lien: "https://www.estiam.education/",
+        img: "https://www.cityguide-dubai.com/fileadmin/_processed_/3/3/csm_img-worlds-of-adventures-teaser_40e4184da1.jpg",
+        title: "Evenement 1",
+        des: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        btn: "Voir Plus",
+    },
+    {
+        col: "col-start-2",
+        lien: "https://www.estiam.education/",
+        img: "https://www.cityguide-dubai.com/fileadmin/_processed_/3/3/csm_img-worlds-of-adventures-teaser_40e4184da1.jpg",
+        title: "Evenement 2",
+        des: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        btn: "Voir Plus",
+    },
+
+    {
+        col: "col-start-3",
+        lien: "https://www.estiam.education/",
+        img: "https://www.cityguide-dubai.com/fileadmin/_processed_/3/3/csm_img-worlds-of-adventures-teaser_40e4184da1.jpg",
+        title: "Evenement 3",
+        des: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        btn: "Voir Plus",
+    }
+]
+
+
+
 //...
