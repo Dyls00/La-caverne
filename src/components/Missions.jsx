741b9f1bbebcdb7c801b365d9fc141a8475d7348
Mathieu LALANDE <mathieu.lalande@estiam.com>
2024-06-13 13:34:03 +0200

    feat/missions : add missions component

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,6 +1,31 @@
+import { missionData, TitleMissionsBDE } from '../constants';
+
 const Missions = () => {
     return (
-        <h1>Hello World Missions</h1>
+        <>
+            <div className="pt-8 px-4 mx-auto max-w-screen-xl text-center lg:pt-16">
+                {TitleMissionsBDE.map((TitleMissionBDE) => (
+                    <>
+                        <h2 className="mb-4 text-xl font-extrabold tracking-tight leading-none text-dark-purple md:text-2xl lg:text-4xl">{TitleMissionBDE.title}</h2>
+                        <p className="mb-8 text-lg font-normal text-white lg:text-xl sm:px-16 lg:px-48 ">{TitleMissionBDE.text}</p>
+                    </>
+                ))}
+            </div>
+
+            <div className="grid mb-8 border rounded-lg shadow-sm md:mb-12 md:grid-cols-2 bg-white mx-auto w-full max-w-screen-2xl">
+                {missionData.map((mission, index) => (
+                    <figure
+                        key={index}
+                        className={`flex flex-col items-center justify-center p-8 bg-white text-center ${index % 2 === 0 ? 'md:border-e' : ''} ${index === 0 ? 'rounded-t-lg md:rounded-t-none md:rounded-ss-lg' : ''} ${index === 1 ? 'md:rounded-se-lg' : ''} ${index === 2 ? 'md:rounded-es-lg md:border-b-0' : ''} ${index === 3 ? 'rounded-b-lg md:rounded-se-lg' : ''}`}
+                    >
+                        <blockquote className="max-w-2xl mx-auto mb-4 text-dark-purple lg:mb-8">
+                            <h3 className="text-lg font-bold text-primary">{mission.title}</h3>
+                            <p className="my-4">{mission.text}</p>
+                        </blockquote>
+                    </figure>
+                ))}
+            </div>
+        </>
     );
 }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
+import { text } from '@fortawesome/fontawesome-svg-core';
 import { faFacebook, faTwitter, faLinkedin, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 export const navLinks = [
@@ -84,4 +85,28 @@ export const socialLinks = [
     },
 ];
 
-//...
+export const TitleMissionsBDE = [
+    {
+        title: "Notre BDE, notre mission",
+        text: "Nous donnons notre âme pour ce BDE, alors souscrivez pour le barbuc"
+    }
+]
+
+export const missionData = [
+    {
+        title: "Organisation d'événements",
+        text: "Le BDE organise divers événements sociaux, culturels, et sportifs pour les étudiants, tels que des soirées, des concerts, des voyages, des compétitions sportives, et des conférences. Ces activités visent à enrichir la vie étudiante et à favoriser la cohésion entre les étudiants."
+    },
+    {
+        title: "Représentation des étudiants",
+        text: "Le BDE agit en tant qu'intermédiaire entre les étudiants et l'administration de l'établissement. Il recueille les opinions et les préoccupations des étudiants et les communique à la direction. Cela inclut également la défense des intérêts des étudiants dans divers comités et conseils de l'institution."
+    },
+    {
+        title: "Gestion des clubs et associations",
+        text: "Le BDE supervise et coordonne les activités des différents clubs et associations étudiantes. Il aide à la création de nouvelles associations, fournit des ressources et des conseils, et peut également allouer des fonds pour soutenir leurs activités."
+    },
+    {
+        title: "Services aux étudiants",
+        text: "YLe BDE peut offrir divers services pour améliorer la vie quotidienne des étudiants, tels que la mise en place de partenariats avec des entreprises locales pour offrir des réductions, l'organisation de ventes de matériel scolaire à prix réduits, ou encore la mise en place de plateformes de covoiturage ou de logement."
+    }
+];
