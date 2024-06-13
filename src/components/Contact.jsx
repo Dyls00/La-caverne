6e877dbc1ecb2647bbb1fbf26dae8e3b39dcc5c8
Mathieu LALANDE <mathieu.lalande@estiam.com>
2024-06-13 17:58:17 +0200

    modifs style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,27 +2,27 @@ const Contact = () => {
     return (
         <div id="Presentation" className="container mx-auto w-full max-w-screen-2xl bg-white py-8 rounded-lg mb-20"> 
             
-                <div class="py-8 lg:py-16  mx-auto max-w-screen-md ">
-                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-dark-purple">Contactez-nous</h2>
-                    <p class="mb-8 lg:mb-16 font-light text-center text-dark-purple sm:text-xl">Si vous avez des questions sur notre BDE ou notre école, n'hésitez pas</p>
-                    <form action="#" class="space-y-8">
+                <div className="py-8 lg:py-16  mx-auto max-w-screen-md ">
+                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-dark-purple">Contactez-nous</h2>
+                    <p className="mb-8 lg:mb-16 font-light text-center text-dark-purple sm:text-xl">Si vous avez des questions sur notre BDE ou notre école, n'hésitez pas</p>
+                    <form action="#" className="space-y-8">
                         <div>
-                            <label for="nomprenom" class="block mb-2 text-sm font-medium text-dark-purple">Votre Nom & Prénom</label>
-                            <input type="text" id="nomprenom" class="shadow-sm bg-gray-50 border border-gray-300 text-dark-purple text-sm rounded-lg focus:ring-dark-purple focus:border-dark-purple block w-full p-2.5 " placeholder="Nom & Prénom" required />
-                        </div>
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-dark-purple">Votre Adresse Mail</label>
-                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-dark-purple text-sm rounded-lg focus:ring-dark-purple focus:border-dark-purple block w-full p-2.5 " placeholder="votremail@estiam.com" required />
+                            <label for="nomprenom" className="block mb-2 text-sm font-medium text-dark-purple">Votre Nom & Prénom</label>
+                            <input type="text" id="nomprenom" className="shadow-sm bg-gray-50 border border-gray-300 text-dark-purple text-sm rounded-lg focus:ring-dark-purple focus:border-dark-purple block w-full p-2.5 " placeholder="Nom & Prénom" required />
                         </div>
                         <div>
-                            <label for="subject" class="block mb-2 text-sm font-medium text-dark-purple">Sujet du Message </label>
-                            <input type="text" id="subject" class="block p-3 w-full text-sm text-dark-purple bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-dark-purple focus:border-dark-purple" placeholder="Objet du Message ..." required />
+                            <label for="email" className="block mb-2 text-sm font-medium text-dark-purple">Votre Adresse Mail</label>
+                            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-dark-purple text-sm rounded-lg focus:ring-dark-purple focus:border-dark-purple block w-full p-2.5 " placeholder="votremail@estiam.com" required />
                         </div>
-                        <div class="sm:col-span-2">
-                            <label for="message" class="block mb-2 text-sm font-medium text-dark-purple">Votre Message</label>
-                            <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-dark-purple bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Corps de Message ..."></textarea>
+                        <div>
+                            <label for="subject" className="block mb-2 text-sm font-medium text-dark-purple">Sujet du Message </label>
+                            <input type="text" id="subject" className="block p-3 w-full text-sm text-dark-purple bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-dark-purple focus:border-dark-purple" placeholder="Objet du Message ..." required />
                         </div>
-                        <button type="submit" class="py-3 px-5 text-sm  text-center text-white rounded-lg bg-orange sm:w-fit hover:bg-dark-purple focus:ring-4 focus:ring-dark-purple">Envoyer</button>
+                        <div className="sm:col-span-2">
+                            <label for="message" className="block mb-2 text-sm font-medium text-dark-purple">Votre Message</label>
+                            <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-dark-purple bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Corps de Message ..."></textarea>
+                        </div>
+                        <button type="submit" className="py-3 px-5 text-sm  text-center text-white rounded-lg bg-orange sm:w-fit hover:bg-dark-purple focus:ring-4 focus:ring-dark-purple">Envoyer</button>
                     </form>
                 </div>
         </div>
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,9 +2,9 @@ import { events, TitleEvent } from "../constants";
 
 const Event = () => {
     return (
-        <div id="Event" className="h-auto lg:h-screen max-w-screen-2xl my-20 violet-gradient">
+        <div id="Event" className="h-auto lg:h-screen w-full my-20 violet-gradient">
             <h2 className="mb-4 text-xl font-extrabold tracking-tight leading-none text-orange md:text-2xl lg:text-4xl py-12 text-center">{TitleEvent.title}</h2>
-            <div className="mx-auto flex flex-col content-center justify-center lg:w-full lg:flex-row lg:justify-between">
+            <div className="flex flex-col content-center justify-center lg:w-full lg:flex-row lg:justify-between">
                 {events.map((event) => (
                     <a key={event.id} href={event.lien} className="w-max m-auto py-4 lg:pt-0">
                         <div className="max-w-sm bg-white border border-white rounded-lg shadow-2xl">
