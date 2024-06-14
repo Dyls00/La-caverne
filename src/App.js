ca86a2ccedf12cd4b795007ab21523ab2e140b29
Mathieu LALANDE <mathieu.lalande@estiam.com>
2024-06-14 09:15:33 +0200

    add .env

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ const App = () => {
         </div>
         <Home />
         <Missions />
-        <Presentation />
         <Event />
+        <Presentation />
         <Contact />
         <Footer />
       </div>
